e73614a11309c864f36873a70f5d5328f8bc1f87
arjun-saseendran <arjunsaseendran@icloud.com>
2024-08-23 04:21:02 +0530

    change class to className

--- src/App.jsx
+++ src/App.jsx
@@ -7,43 +7,32 @@ function App() {
   useEffect(() => {
     fetch("https://fakestoreapi.com/products")
       .then((res) => res.json())
-      .then((product) => {setProducts(product)
+      .then((product) => {
+        setProducts(product);
         console.log(product);
-        
       });
   }, []);
 
   return (
     <>
-    <div className="row container">
-      {products.map((product, index) => {
-        return (
-          
-          <div className="container row">
-<div col-12 col-sm-6 col-md-4 col-lg-3>
-<div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="..."/>
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text"></p>
-    <a href="#" class="btn btn-primary">Add to cart</a>
-  </div>
-</div>
-
-
-
-</div>
-
-
-
-
+      <div className="row container">
+        {products.map((product, index) => {
+          return (
+            <div key={index} className=" col-12 col-sm-6 col-md-4 col-lg-3">
+              <div className="card" style={{width: 18 rem}}>
+                <img src={product.image} className="card-img-top" alt="product-image" />
+                <div className="card-body">
+                  <h5 className="card-title">{product.title}</h5>
+                  <p className="card-text">{product.description}</p>
+                  <a href="#" className="btn btn-primary">
+                    Add to cart
+                  </a>
+                </div>
+              </div>
             </div>
-          
-        )
-      })}
-          </div>
-      
-     
+          );
+        })}
+      </div>
     </>
   );
 }

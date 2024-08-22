82e5154574fd31bce70eaec9c43e97cf44c52286
arjun-saseendran <arjunsaseendran@icloud.com>
2024-08-23 05:20:26 +0530

    change nav bar bg to bg senondary, text color to white, add nvabar active class

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,25 +3,25 @@ import React from 'react'
 function Header() {
   return (
     <>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+    <nav className="navbar navbar-expand-lg bg-secondary">
+  <div className="container-fluid">
+    <a className="navbar-brand text-white" href="#">Shopkart</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a className="nav-link active text-white" aria-current="page" href="#">Home</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
+        <li className="nav-item">
+          <a className="nav-link active text-white" href="#">Account</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
+        <li className="nav-item">
+          <a className="nav-link active text-white" href="#">Cart</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+        <li className="nav-item">
+          <a className="nav-link active text-white" aria-disabled="true" href='#'>Settings</a>
         </li>
       </ul>
     </div>

e74e8a6dbdc4f4a5ac9b9a55e6028a19730f9cb9
arjun-saseendran <arjunsaseendran@icloud.com>
2024-08-23 05:29:59 +0530

    add header, main, and footer components to app

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <>
+    <div className='bg-secondary text-white p-3' style={{height: '3rem'}}>arjunsaseendran&#169;recived</div>
+    </>
+  )
+}
+
+export default Footer

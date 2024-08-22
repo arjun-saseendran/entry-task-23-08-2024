33b31b459e5e9ea3670115b52612d10c679aa13d
arjun-saseendran <arjunsaseendran@icloud.com>
2024-08-23 05:05:52 +0530

    add bootstrap nav barcomponent to Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,11 @@
-import React, { useState, useEffect } from "react";
 
-import "./App.css";
 
 function App() {
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    fetch("https://fakestoreapi.com/products")
-      .then((res) => res.json())
-      .then((product) => {
-        setProducts(product);
-        console.log(product);
-      });
-  }, []);
-
-  return (
+  return(
     <>
-     
     </>
-  );
+  )
+
 }
 
 export default App;
--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,33 +1,44 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
 function Main() {
-  return (
-    <>
-     <div className="container row p-5">
-        {products.map((product, index) => {
-          return (
-            <div key={index} className=" col-12 col-sm-6 col-md-4 col-lg-3 mt-3">
-              <div className="card overfolw-hidden" style={{ width: "18rem", height:'30rem'}}>
-                <img
-                  src={product.image}
-                  className="card-img-top object-fit-contain p-3 "
-                  style={{height: '200px'}} alt="product-image"
-                />
-                <div className="card-body" style={{height:'25rem', overflow:'hidden'}}>
-                  <h5 className="card-title text-white" style={{height:'3rem', overflow: 'hidden'}}>{product.title}</h5>
+    const [products, setProducts] = useState([]);
 
-                  <p className="card-text overflow:hidden" style={{height:'6rem', overflow: 'hidden'}}>{product.description}</p>
-                  <h5 className="card-title text-white">${product.price}</h5>
-                  <a href="#" className="btn btn-secondary">
-                    Add to cart
-                  </a>
+    useEffect(() => {
+      fetch("https://fakestoreapi.com/products")
+        .then((res) => res.json())
+        .then((product) => {
+          setProducts(product);
+          console.log(product);
+        });
+    }, []);
+  
+    return (
+      <>
+        <div className="container row p-5">
+          {products.map((product, index) => {
+            return (
+              <div key={index} className=" col-12 col-sm-6 col-md-4 col-lg-3 mt-3">
+                <div className="card overfolw-hidden" style={{ width: "18rem", height:'30rem'}}>
+                  <img
+                    src={product.image}
+                    className="card-img-top object-fit-contain p-3 "
+                    style={{height: '200px'}} alt="product-image"
+                  />
+                  <div className="card-body" style={{height:'25rem', overflow:'hidden'}}>
+                    <h5 className="card-title text-white" style={{height:'3rem', overflow: 'hidden'}}>{product.title}</h5>
+  
+                    <p className="card-text overflow:hidden" style={{height:'6rem', overflow: 'hidden'}}>{product.description}</p>
+                    <h5 className="card-title text-white">${product.price}</h5>
+                    <a href="#" className="btn btn-secondary">
+                      Add to cart
+                    </a>
+                  </div>
                 </div>
               </div>
-            </div>
-          );
-        })}
-      </div>
-    </>
+            );
+          })}
+        </div>
+      </>
   )
 }
 

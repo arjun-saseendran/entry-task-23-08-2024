d40a60e55e965ddf93531c5ee2579300b07de18f
arjun-saseendran <arjunsaseendran@icloud.com>
2024-08-23 06:10:15 +0530

    add container fulid class, bux fix

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <>
-    <div className='bg-secondary text-white p-3' style={{height: '3rem'}}>
+    <div className='bg-secondary text-white p-4' style={{height: '4rem'}}>
     Copyright © 2024 arjunsaseendran.</div>
     </>
   )
--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,45 +1,67 @@
 import React, { useState, useEffect } from "react";
 
 function Main() {
-    const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([]);
 
-    useEffect(() => {
-      fetch("https://fakestoreapi.com/products")
-        .then((res) => res.json())
-        .then((product) => {
-          setProducts(product);
-          console.log(product);
-        });
-    }, []);
-  
-    return (
-      <>
-        <div className="container row p-5">
-          {products.map((product, index) => {
-            return (
-              <div key={index} className=" col-12 col-sm-6 col-md-4 col-lg-3 mt-3">
-                <div className="card overfolw-hidden" style={{ width: "18rem", height:'30rem'}}>
-                  <img
-                    src={product.image}
-                    className="card-img-top object-fit-contain p-3 "
-                    style={{height: '200px'}} alt="product-image"
-                  />
-                  <div className="card-body" style={{height:'25rem', overflow:'hidden'}}>
-                    <h5 className="card-title text-secondary" style={{height:'3rem', overflow: 'hidden'}}>{product.title}</h5>
-  
-                    <p className="card-text overflow:hidden text-secondary" style={{height:'6rem', overflow: 'hidden'}}>{product.description}</p>
-                    <h5 className="card-title text-secondary">${product.price}</h5>
-                    <a href="#" className="btn btn-secondary">
-                      Add to cart
-                    </a>
-                  </div>
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((product) => {
+        setProducts(product);
+        console.log(product);
+      });
+  }, []);
+
+  return (
+    <>
+    <div className="container">
+      <div className="row mx-auto mt-5">
+        {products.map((product, index) => {
+          return (
+            <div key={index} className=" col-12 col-sm-6 col-md-4 col-lg-3">
+              <div
+                className="card overfolw-hidden mb-4"
+                style={{ width: "18rem", height: "30rem" }}
+              >
+                <img
+                  src={product.image}
+                  className="card-img-top object-fit-contain"
+                  style={{ height: "200px" }}
+                  alt="product-image"
+                />
+                <div
+                  className="card-body"
+                  style={{ height: "25rem", overflow: "hidden" }}
+                >
+                  <h5
+                    className="card-title text-secondary"
+                    style={{ height: "3rem", overflow: "hidden" }}
+                  >
+                    {product.title}
+                  </h5>
+
+                  <p
+                    className="card-text overflow:hidden text-secondary"
+                    style={{ height: "6rem", overflow: "hidden" }}
+                  >
+                    {product.description}
+                  </p>
+                  <h5 className="card-title text-secondary">
+                    ${product.price}
+                  </h5>
+                  <a href="#" className="btn btn-secondary">
+                    Add to cart
+                  </a>
                 </div>
               </div>
-            );
-          })}
-        </div>
-      </>
-  )
+            </div>
+            
+          );
+        })}
+      </div>
+      </div>
+    </>
+  );
 }
 
-export default Main
+export default Main;
